4fa37af608bf8c3a03481dff3c7bcd096a5f3145
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-17 17:11:51 -0600

    GoFish fish food update for arm64-cgo version v1.0.35

--- Food/ipsw.lua
+++ Food/ipsw.lua
@@ -1,5 +1,5 @@
 local name = "ipsw"
-local version = "3.0.90"
+local version = "1.0.35"
 
 food = {
     name = name,
@@ -9,64 +9,14 @@ food = {
     version = version,
     packages = {
         {
-            os = "windows",
-            arch = "arm64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_windows_arm64.zip",
-            sha256 = "2fc3f9ec495f70255d8ca355ac0eb3b835bcf832b05f419f3529799f652ec73d",
-            resources = {
-                {
-                    path = "ipsw.exe",
-                    installpath = "bin\\ipsw.exe",
-                },
-            }
-        },
-        {
-            os = "linux",
-            arch = "arm64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_linux_arm64.tar.gz",
-            sha256 = "c0a209521db5e6df3ed1c907ffb36a4ffe575943ea1a65ea30856a666b895a80",
-            resources = {
-                {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
-                    executable = true
-                },
-            }
-        },
-        {
-            os = "windows",
-            arch = "amd64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_windows_x86_64.zip",
-            sha256 = "498c03f8fa4b920fb426e082f0e9705aabc559d0bf3f7ae1690284621ffa8f73",
-            resources = {
-                {
-                    path = "ipsw.exe",
-                    installpath = "bin\\ipsw.exe",
-                },
-            }
-        },
-        {
-            os = "linux",
-            arch = "amd64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_linux_x86_64.tar.gz",
-            sha256 = "b057e1a1a605ec7ff8660269d46532568a32d8175bffece0937fb9f4570e3fcb",
-            resources = {
-                {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
-                    executable = true
-                },
-            }
-        },
-        {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_macOS_arm64.tar.gz",
-            sha256 = "054e03136bb1d3dbd427247244e7403f87d0f4e2047f3c5d0952e7de55e51117",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_arm64.tar.gz",
+            sha256 = "8830ea30f6dcfc2e5e98a1cc68e3ea0f94ef93be077b72e257e42c7e85b46d19",
             resources = {
                 {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
+                    path = "disass",
+                    installpath = "bin/disass",
                     executable = true
                 },
             }
@@ -74,12 +24,12 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_macOS_x86_64.tar.gz",
-            sha256 = "d5c022a00553dcf5ac7321f53ca1124d0cbbeb296ae5bcf627f23f9c512ced13",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_x86_64.tar.gz",
+            sha256 = "165843b8642d743a2df4f8dda0ea2c409a7f6d9e8403ac40f8dc8bc50666b994",
             resources = {
                 {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
+                    path = "disass",
+                    installpath = "bin/disass",
                     executable = true
                 },
             }
@@ -87,12 +37,12 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_macOS_universal.tar.gz",
-            sha256 = "d5edb8ac1e469ced6a8086a400c228b727e20050de521948dee4f3001a795e72",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_universal.tar.gz",
+            sha256 = "c91078c560d76fed7545250f68532cd15f44d1b21e5cc987050c05a4ac861a11",
             resources = {
                 {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
+                    path = "disass",
+                    installpath = "bin/disass",
                     executable = true
                 },
             }
@@ -100,12 +50,12 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.0.90/ipsw_3.0.90_macOS_universal.tar.gz",
-            sha256 = "d5edb8ac1e469ced6a8086a400c228b727e20050de521948dee4f3001a795e72",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_universal.tar.gz",
+            sha256 = "c91078c560d76fed7545250f68532cd15f44d1b21e5cc987050c05a4ac861a11",
             resources = {
                 {
-                    path = "ipsw",
-                    installpath = "bin/ipsw",
+                    path = "disass",
+                    installpath = "bin/disass",
                     executable = true
                 },
             }
