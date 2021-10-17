11e8015f544bcd3207413d2d6ee116df41c0c885
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-17 17:20:08 -0600

    GoFish fish food update for arm64-cgo version v1.0.36

--- Food/ipsw.lua
+++ Food/ipsw.lua
@@ -1,5 +1,5 @@
 local name = "ipsw"
-local version = "1.0.35"
+local version = "1.0.36"
 
 food = {
     name = name,
@@ -10,35 +10,9 @@ food = {
     packages = {
         {
             os = "darwin",
-            arch = "arm64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_arm64.tar.gz",
-            sha256 = "8830ea30f6dcfc2e5e98a1cc68e3ea0f94ef93be077b72e257e42c7e85b46d19",
-            resources = {
-                {
-                    path = "disass",
-                    installpath = "bin/disass",
-                    executable = true
-                },
-            }
-        },
-        {
-            os = "darwin",
             arch = "amd64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_x86_64.tar.gz",
-            sha256 = "165843b8642d743a2df4f8dda0ea2c409a7f6d9e8403ac40f8dc8bc50666b994",
-            resources = {
-                {
-                    path = "disass",
-                    installpath = "bin/disass",
-                    executable = true
-                },
-            }
-        },
-        {
-            os = "darwin",
-            arch = "amd64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_universal.tar.gz",
-            sha256 = "c91078c560d76fed7545250f68532cd15f44d1b21e5cc987050c05a4ac861a11",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.36/disass_1.0.36_macOS_universal.tar.gz",
+            sha256 = "ca4f9c0bbe0b0e5c81dada68965b103c14093d8e4b93bf3e6091988f0b51d510",
             resources = {
                 {
                     path = "disass",
@@ -50,8 +24,8 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.35/disass_1.0.35_macOS_universal.tar.gz",
-            sha256 = "c91078c560d76fed7545250f68532cd15f44d1b21e5cc987050c05a4ac861a11",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.36/disass_1.0.36_macOS_universal.tar.gz",
+            sha256 = "ca4f9c0bbe0b0e5c81dada68965b103c14093d8e4b93bf3e6091988f0b51d510",
             resources = {
                 {
                     path = "disass",
