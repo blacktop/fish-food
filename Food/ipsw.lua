de8d867440557c6908a03801c90abbf689cc322a
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-29 23:43:12 -0700

    GoFish fish food update for ipsw version v3.1.36

--- Food/ipsw.lua
+++ Food/ipsw.lua
@@ -1,5 +1,5 @@
 local name = "ipsw"
-local version = "3.1.35"
+local version = "3.1.36"
 
 food = {
     name = name,
@@ -11,8 +11,8 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.1.35/ipsw_3.1.35_macOS_arm64.tar.gz",
-            sha256 = "9f2bd7a5ee7ebaacc54531718aeb57d5a5fd52bce0f3385d137fc86bf502fa16",
+            url = "https://github.com/blacktop/ipsw/releases/download/v3.1.36/ipsw_3.1.36_macOS_arm64.tar.gz",
+            sha256 = "ad1b5eaa8da53523b9a3d9be8cf08eeb07a63eea34e492b7dee402bf291af275",
             resources = {
                 {
                     path = "ipsw",
@@ -24,8 +24,8 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/blacktop/ipsw/releases/download/v3.1.35/ipsw_3.1.35_macOS_x86_64.tar.gz",
-            sha256 = "4af5c5fdf6fc2aad7d718c9ac04fdc8a3d722eb5750586ae402ea33ce44a0bf7",
+            url = "https://github.com/blacktop/ipsw/releases/download/v3.1.36/ipsw_3.1.36_macOS_x86_64.tar.gz",
+            sha256 = "70addfc6e0c1282e6ecfaa42f28f49cab0cc951ccb6c1b62c9e3c077fc967ce1",
             resources = {
                 {
                     path = "ipsw",
