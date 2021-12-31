75c0d35bbafef7f2ec34db031ac9bc7877489aed
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-31 15:21:31 -0700

    GoFish fish food update for arm64-cgo version v1.0.50

--- Food/disass.lua
+++ Food/disass.lua
@@ -1,5 +1,5 @@
 local name = "disass"
-local version = "1.0.49"
+local version = "1.0.50"
 
 food = {
     name = name,
@@ -11,8 +11,8 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.49/disass_1.0.49_macOS_universal.tar.gz",
-            sha256 = "09376069b40cbd7b0d5ba035a5371204552802b27ee60c13de030c552522582e",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.50/disass_1.0.50_macOS_universal.tar.gz",
+            sha256 = "dab5da47f8185f4dbbb2d3554c20fc198689c5a175e5e843742d245274f3902c",
             resources = {
                 {
                     path = "disass",
@@ -24,8 +24,8 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.49/disass_1.0.49_macOS_universal.tar.gz",
-            sha256 = "09376069b40cbd7b0d5ba035a5371204552802b27ee60c13de030c552522582e",
+            url = "https://github.com/blacktop/arm64-cgo/releases/download/v1.0.50/disass_1.0.50_macOS_universal.tar.gz",
+            sha256 = "dab5da47f8185f4dbbb2d3554c20fc198689c5a175e5e843742d245274f3902c",
             resources = {
                 {
                     path = "disass",
