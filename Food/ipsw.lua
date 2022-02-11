3eedef8e01236e1c720cc1c69127404a0b562cdf
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-10 22:00:28 -0700

    GoFish fish food update for ipsw version v3.1.62

--- Food/ipsw.lua
+++ Food/ipsw.lua
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = "https://github.com/blacktop/ipsw/releases/download/v3.1.62/ipsw_macOS_arm64.tar.gz",
-            sha256 = "3955379bfa73e9c1158facc4c30fcb237cdca2ef3f06fae69234a883cb133f37",
+            sha256 = "dcfe495c5e64a498e962f79c72dd637133526cbc91486e8f9059fb0d21c42a06",
             resources = {
                 {
                     path = "ipsw",
@@ -25,7 +25,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/blacktop/ipsw/releases/download/v3.1.62/ipsw_macOS_x86_64.tar.gz",
-            sha256 = "4a2b7f871243a2b73026e8f60261481777521f6fd10ca401d86b80965a15a9af",
+            sha256 = "e4417f2f6a87013bf2c74a4e3d7f76dbd4f5c70dca89a9e884d532f6c3bf43f2",
             resources = {
                 {
                     path = "ipsw",
